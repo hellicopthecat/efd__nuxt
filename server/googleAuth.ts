beba3120b4bd3769721a85a5f4d9e4ef0fcb26ef
Hellicat <jhccop@naver.com>
2025-02-21 15:57:56 +0900

    🚑️ Fix_GoogleOauth_Redirect
    Fix_GoogleOauth_Redirect

--- server/googleAuth.ts
+++ server/googleAuth.ts
@@ -5,7 +5,7 @@ export const initGoogleAuth = () => {
   const oauth2Client = new google.auth.OAuth2(
     config.public.GOOGLE_OAUTH_CLIENT,
     config.GOOGLE_OAUTH_CLIENT_SECREY,
-    "http://localhost:3000/api/auth/social/googleRedirect"
+    "https://efd.netlify.app/api/auth/social/googleRedirect"
   );
   return oauth2Client;
 };
